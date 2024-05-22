fef601a4205f0f4825bbc5c08f047f36690fdd11
akshardesai212004 <akshardesai.bca2022@iar.ac.in>
2024-05-22 16:16:53 +0530

    made a commit hehhehe

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 console.log("hello hitesh");
 
+var a=0;
+
+console.log("below is the code for printing 1 to 20 numbers using while loop");
+
+while(a<=20){
+    console.log(a);
+    a++;
+
+
+    
+}
